ccf69386c2e57eb780fda410ff2d0682a326ee55
Olena Halaiko <lenagalayko66@gmail.com>
2022-11-11 17:13:35 +0200

    changed last 2 lines - text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Зміни тексту");
 Console.WriteLine("у перших");
 Console.WriteLine("3-х рядках.");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Зміни тексту");
+Console.WriteLine("у  2-х останніх рядках.");
