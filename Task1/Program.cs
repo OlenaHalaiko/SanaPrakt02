ea00ee074dc885189cf039f6270c5453e8908287
Olena Halaiko <lenagalayko66@gmail.com>
2022-11-11 17:11:54 +0200

    changed first 3 lines - text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Зміни тексту");
+Console.WriteLine("у перших");
+Console.WriteLine("3-х рядках.");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
